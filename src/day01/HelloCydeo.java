e592a5615a9445f5b40c58f919eb34c8e6c9aaa1
HelloWord <HelloWorld@>
2026-09-17 17:33:56 -0400

    HelloWorld and HelloCydeo Updated

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -3,6 +3,6 @@ package day01;
 public class HelloCydeo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Cydeo");
     }
 }
